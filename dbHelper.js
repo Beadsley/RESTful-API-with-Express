b66771ddab6edc668ec9e5d0cc6a8267ed08d869
beadsley <danbeadleson@gmail.com>
2020-02-18 17:53:40 +0100

    refractored

--- dbHelper.js
+++ dbHelper.js
@@ -3,7 +3,9 @@ const mongodb = require('mongodb');
 
 const getDocuments = (db) => {
     return new Promise((resolve, reject) => {
+
         const collection = db.collection('documents');
+
         collection.find({}).toArray(function (err, docs) {
             if (err) {
                 reject(err);
@@ -11,10 +13,11 @@ const getDocuments = (db) => {
             resolve(docs);
         });
     })
-}
+};
 
 const updateDocument = (db, id, obj) => {
     return new Promise((resolve, reject) => {
+
         const collection = db.collection('documents');
         const myquery = { _id: new mongodb.ObjectID(id) };
 
@@ -29,7 +32,7 @@ const updateDocument = (db, id, obj) => {
                 }   
             });
     })
-}
+};
 
 const insertDocuments = function (db, obj) {
 
@@ -42,30 +45,30 @@ const insertDocuments = function (db, obj) {
                 console.log(`Inserted "${obj.name}" document into the collection`);
             }
         });
-}
+};
 
 const removeAll = function (db, callback) {
-    // Get the documents collection
+
     const collection = db.collection('documents');
-    // Delete document where a is 3
+
     collection.deleteMany({}, function (err, result) {
         assert.equal(err, null);
         console.log("Removed everything");
         callback(result);
     });
-}
+};
 
 const remove = function (db, id) {
-    // Get the documents collection
+
     const collection = db.collection('documents');
-    // Delete document where a is 3    
     const myquery = { _id: new mongodb.ObjectID(id) };
+
     collection.deleteOne(myquery, function (err) {
         if (!err) {
             console.log(`Removed ${id}`);
         }
     });
-}
+};
 
 module.exports = {
     getDocuments,
